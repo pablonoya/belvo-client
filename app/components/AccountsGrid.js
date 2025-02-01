1df7f0c4e3e54420b29932a8d30cfc253c9d1929
pablonoya <pablonoya18@live.com>
2025-01-31 20:39:36 -0400

    Add Accounts Grid

---
+++ app/components/AccountsGrid.js
@@ -0,0 +1,34 @@
+"use client"
+import Link from "next/link"
+
+import { useFetch } from "@/hooks/useFetch"
+import Loading from "./Loading"
+
+export default function AccountsGrid({ institution }) {
+  const { data, loading, error } = useFetch("accounts?institution=" + institution)
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <span className="text-red-500">Error loading account: {error.message || error}</span>
+  }
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {data.results.map(account => (
+        <Link
+          href={`/transactions?link=${account.link}&account=${account.id}`}
+          key={account.id}
+          className="border-2 border-gray-400 p-4 rounded-lg flex flex-col gap-1"
+        >
+          <p className="font-bold text-lg">{account.name}</p>
+          <p>
+            ${account.balance.current} {account.currency}
+          </p>
+        </Link>
+      ))}
+    </div>
+  )
+}
---
+++ app/institution/[slug]/accounts/page.js
@@ -0,0 +1,14 @@
+import AccountsGrid from "@/app/components/AccountsGrid"
+import BackButton from "@/app/components/BackButton"
+
+export default async function Accounts({ params }) {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex space-x-2">
+        <BackButton />
+        <h1 className="text-2xl font-bold">Cuentas</h1>
+      </div>
+      <AccountsGrid institution={params.slug} />
+    </div>
+  )
+}
